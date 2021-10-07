164f3282065ab9eb8030aa5fa82654a94b1b25a0
Uzi-Cohen <84247101+Uzi-Cohen@users.noreply.github.com>
2021-10-07 08:09:24 +0300

    Add files via upload
    all answers are included in the app.js

---
+++ day1/app.js
@@ -0,0 +1,27 @@
+// 5 + "34" =534
+// 5 - "4" =1
+// 10 % 5 =0
+// 5 % 10 =5
+// "Java" + "Script" =JavaScript
+// " " + " "= '  '
+// " " + 0 = ' 0'
+// true + true = 2
+// true + false = 1
+// false + true = 1
+// false - true = -1
+// 3 - 4 = -1
+// "Bob" - "bill" ='NaN'
+
+
+
+
+
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+fruits.shift();
+fruits.reverse();
+fruits.pop()
+fruits.push('Kiwi')
+fruits.reverse()
+
+let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+moreFruits.
