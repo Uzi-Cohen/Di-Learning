d06b7a5aba83f63aa0831f143dcea4bb9828ddea
Uzi-Cohen <84247101+Uzi-Cohen@users.noreply.github.com>
2021-10-11 09:31:07 +0300

    Update app.js

--- jS day 2/app.js
+++ jS day 2/app.js
@@ -1,26 +1,51 @@
-dailb challenges javascript dab 3
-// let n = 6;
-// let star = "";
-// for (let i = 1; i <= n; i++) {
-//   for (let j = 0; j < i; j++) {
-//     star += "*";
-//   }
-//   star += "\n";
-// }
-// console.log(star);
+daily challenges javascript day 2
 
-// var a,b,str;
-// for(a=1; a <=7; a++)
-// {
-//    for (b=1; b < a; b++)
-//      {
-//     str=str+("*");
-//       }
-//  console.log(str);
-//  str='';
+
+
+// let x = 9
+// let y = 11
+//
+// if (y > x) {
+//   console.log('y is the bigger number')
+// }
+//
+// else if (x > y) {
+//   console.log('x is the bigger number')
+// }
+// else {
+//   console.log('how?')
 // }
 
-// const numbers = [5,0,9,1,7,4,2,6,3,8];
-// numbers.toString();
-// numbers.join(' ');
-// not doing that bonus haha
+// var newDog = ('chihuahua')
+// newDog.length()
+// newDog.toUpperCase()
+//
+// if (newDog = 'chihuahua') {
+//   console.log('I love Chihuahuas, it’s my favorite dog breed')
+// }
+// else {
+//   console.log('huskies are better anyways')
+// }
+
+// let number = prompt('number please')
+// if (number%2 == 0) {
+//   console.log('it is even')
+// }
+// else {
+//   console.log('it is odd')
+// }
+
+// let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"]
+// if (users.length = 0) {
+//   console.log('no one is online')
+// }
+// else if (users.length = 1) {
+//   console.log('Princess45')
+// }
+// else if (users.length > 1) {
+//   console.log('"Princess45", "cat&doglovers" are online '')
+// }
+// var sentance = "this is not so bad";
+// var wordNot = sentance.slice(8, 11);
+// var wordBad = sentance.slice(15, 18);
+// if ()
