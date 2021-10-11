6fac5b1c4b31d91d4b8aaf422d2982ca09392882
Uzi-Cohen <84247101+Uzi-Cohen@users.noreply.github.com>
2021-10-11 09:32:38 +0300

    Add files via upload

---
+++ jS day 3/app.js
@@ -0,0 +1,29 @@
+
+
+
+daily challenges javascript day 3
+// let n = 6;
+// let star = "";
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 0; j < i; j++) {
+//     star += "*";
+//   }
+//   star += "\n";
+// }
+// console.log(star);
+
+// var a,b,str;
+// for(a=1; a <=7; a++)
+// {
+//    for (b=1; b < a; b++)
+//      {
+//     str=str+("*");
+//       }
+//  console.log(str);
+//  str='';
+// }
+
+// const numbers = [5,0,9,1,7,4,2,6,3,8];
+// numbers.toString();
+// numbers.join(' ');
+// not doing that bonus haha
