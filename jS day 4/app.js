4bedbdd5cc78b35edf62dd2dd3bc09e7cdcabfb1
Uzi-Cohen <84247101+Uzi-Cohen@users.noreply.github.com>
2021-10-12 10:49:49 +0300

    Add files via upload
    this is still a work in progress

---
+++ jS day 4/app.js
@@ -0,0 +1,75 @@
+// day 4 challenges. functions
+// #1 pt1
+// function infoAboutMe() {
+//   console.log('my name is Uzi')
+// }
+// infoAboutMe()
+//
+// #1 pt2
+// function infoAboutPerson(personName, personAge, personFavoriteColor) {
+//   return(`hey your name is, ${personName}, you are ${personAge} years old, your favoritecolor is, ${personFavoriteColor}`)
+// }
+//
+// #1 pt3
+
+// function infoAboutPerson(personName, personAge, personFavoriteColor, personHobbies) {
+//   return(`hey your name is, ${personName}, you are ${personAge} years old, your favoritecolor is, ${personFavoriteColor}`)
+//console.log
+// }
+
+// #2 keyless car
+// function checkDriverAge () {
+//     var age = prompt("What is your age?");
+// if (age >= 18) {
+//   console.log('You are old enough to drive, Powering On. Enjoy the ride!');
+// } else if (age < 18) {
+//   alert("sorry you are too young yo drive this.");
+// } else {
+//   alert("Congratulations on your first year of driving. Enjoy the ride");
+// }
+// }
+// checkDriverAge()
+//
+// #3
+// function checkNumber() {
+//   for (let i = 0;
+// i <= 100;
+// i++) {
+//   if (i % 2 == 0) {
+//     console.log('this number is even')
+//   } else {
+//     console.log('this number is odd')
+//   }
+// }
+// }
+// checkNumber()
+//
+// #4
+// function isDivisible() {
+//   for (let i = 0;
+// i <= 500;
+// i++) {
+//   if (i % 23 == 0) {
+//     results.push(i)
+//   } else {
+//     sum += i
+//   }
+// }
+// }
+// this one is very complicated.
+// #5
+// let amazonBasket = {
+//     glasses: 1,
+//     books: 2,
+//     floss: 100
+// }
+// var item = prompt('please select an item').toLowerCase()
+//
+// function checkBasket(amazonBasket) {
+// if (item in amazonBasket=== true) {
+//   console.log('we have that :)')
+// }
+// } dont really understand
+
+// #6 wallet
+// var item =
