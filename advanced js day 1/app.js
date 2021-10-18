47b63b07dbda2c23d26a3d655797096d038c36ea
Uzi-Cohen <84247101+Uzi-Cohen@users.noreply.github.com>
2021-10-18 09:19:29 +0300

    Add files via upload

---
+++ advanced js day 1/app.js
@@ -0,0 +1,30 @@
+const gameInfo = [
+ {
+   username: "john",
+   team: "red",
+   score: 5,
+   items: ["ball", "book", "pen"]
+ },
+ {
+   username: "becky",
+   team: "blue",
+   score: 10,
+   items: ["tape", "backpack", "pen"]
+ },
+ {
+   username: "susy",
+   team: "red",
+   score: 55,
+   items: ["ball", "eraser", "pen"]
+ },
+ {
+   username: "tyson",
+   team: "green",
+   score: 1,
+   items: ["book", "pen"]
+ },
+];
+var double = [];
+gameInfo.forEach(person => double.push(person.username + "!"));
+
+console.log(double);
