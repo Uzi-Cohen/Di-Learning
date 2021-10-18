d2c581d37a4335549fccf0723b4d0459ad4c27c8
Uzi-Cohen <84247101+Uzi-Cohen@users.noreply.github.com>
2021-10-18 09:37:27 +0300

    Update app.js

--- advanced js day 1/app.js
+++ advanced js day 1/app.js
@@ -28,3 +28,94 @@ var double = [];
 gameInfo.forEach(person => double.push(person.username + "!"));
 
 console.log(double);
+// // #1
+// function q1() {
+//     var a = 5;
+//     if(a > 1) {
+//         a = 3;
+//     }
+//     alert(a);
+// }
+//
+// //#2
+// var a = 0;
+// function q2() {
+//     a = 5;
+// }
+//
+// function q22() {
+//     alert(a);
+// }
+//
+//
+// //#3
+// function q3() {
+//     window.a = "hello";
+// }
+//
+//
+// function q32() {
+//     alert(a);
+// }
+//
+// //#4
+// var a = 1;
+// function q4() {
+//     var a = "test";
+//     alert(a);
+// }
+//
+// //#5
+// var a = 2;
+// if (true) {
+//     var a = 5;
+//     alert(a);
+// }
+// alert(a);
+// the a will comee out as 5
+
+// function winBattle(){
+//     return true;
+// }
+//
+// function experiencePoints() {
+//     if (winBattle()) {
+//         return 10;
+//     } else {
+//         return 1;
+//     }
+// }
+//
+// experiencePoints()
+//
+// var experiencePoints = 10;
+// console.log((experiencePoints >= 10))? 'you win the battle!' : 'you lose!'
+// #3
+// var colors = ["red","orange","yellow","green","blue","violet"];
+// for (const color of colors){
+//     console.log(color)
+//     }
+//
+// #4
+// var color = ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow "];
+//
+// function Ordinal(n)
+//  {
+//  var o = ["th","st","nd","rd"],
+//  x = n%100;
+//  return x+(o[(x-20)%10]||o[x]||o[0]);
+//  }
+//
+// for(n = 0; n < color.length; n++){
+//  var ordinal = n + 1;
+//
+//  var output = (Ordinal(ordinal) + " choice is " + color[n] + ".");
+//
+// console.log(output);
+// }
+
+// #5
+// if (typeof myVar === 'string' || myVar instanceof String)
+// // it's a string
+// else{
+// // it's something else}
