ed77b25439ec50abad90e758e04989969261453c
Uzi-Cohen <84247101+Uzi-Cohen@users.noreply.github.com>
2021-10-18 08:51:19 +0300

    Add files via upload
    hi for some reason I'm losing many of my files, some arent uploading

---
+++ dom day 3-1/app.js
@@ -0,0 +1,12 @@
+// function validate() {
+//     if (document.myForm.name.value == "") {
+//         alert("Enter a name");
+//         document.myForm.name.focus();
+//         return false;
+//     }
+//     if (!/^[a-zA-Z]*$/g.test(document.myForm.name.value)) {
+//         alert("Invalid characters");
+//         document.myForm.name.focus();
+//         return false;
+//     }
+// }
