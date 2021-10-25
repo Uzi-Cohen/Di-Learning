8af952ab994388f2a138beac75474182a6b14b17
Uzi-Cohen <84247101+Uzi-Cohen@users.noreply.github.com>
2021-10-25 12:10:36 +0300

    Add files via upload

---
+++ advanced_JS_3/app.js
@@ -0,0 +1,54 @@
+//#1
+// ------1------
+// const fruits = ["apple", "orange"];
+// const vegetables = ["carrot", "potato"];
+//
+// const result = ['bread', ...vegetables, 'chicken', ...fruits];
+// console.log(result);
+// result: ['bread', 'carrot', 'potato', 'chicken', 'apple', 'orange']
+// ------2------
+// const country = "USA";
+// console.log([...country]);
+// result: ['U', 'S', 'A']
+// ------Bonus------
+// let newArray = [...[,,]];
+// console.log(newArray);
+// result:[undefined, undefined]
+
+
+// #2
+// let users = [{ firstName: 'Bradley', lastName: 'Bouley', role: 'Full Stack Resident' },
+//              { firstName: 'Chloe', lastName: 'Alnaji', role: 'Full Stack Resident' },
+//              { firstName: 'Jonathan', lastName: 'Baughn', role: 'Enterprise Instructor' },
+//              { firstName: 'Michael', lastName: 'Herman', role: 'Lead Instructor' },
+//              { firstName: 'Robert', lastName: 'Hajek', role: 'Full Stack Resident' },
+//              { firstName: 'Wes', lastName: 'Reid', role: 'Instructor'},
+//              { firstName: 'Zach', lastName: 'Klabunde', role: 'Instructor'}];
+            //pt1
+            // let mapped = users.map(function (user) {
+            // return user.firstName;
+            // });
+            // return ('welcome ' + mapped)
+            //pt2
+            // users.filter(function (user) {
+            // return user.role === 'Full Stack Resident';
+            // });
+
+
+      //#3
+      // var epic = ['a', 'long', 'time', 'ago', 'in a', 'galaxy', 'far far', 'away'];
+      // epic.reduce(function (previous, current) {
+      // return previous + ' ' + current;
+      // });
+
+      // let student = [{name: "Ray", course: "Computer Science", isPassed: true},
+      //    {name: "Liam", course: "Computer Science", isPassed: false},
+      //    {name: "Jenner", course: "Information Technology", isPassed: true},
+      //    {name: "Marco", course: "Robotics", isPassed: true},
+      //    {name: "Kimberly", course: "Artificial Intelligence", isPassed: false},
+      //    {name: "Jamie", course: "Big Data", isPassed: false}];
+
+      // student.filter(function (user) {
+      // return student.isPassed === true;
+      // });
+      //
