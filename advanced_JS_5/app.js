0050ef6d121698fbd4ab49f1e74271de254515b9
Uzi-Cohen <84247101+Uzi-Cohen@users.noreply.github.com>
2021-10-26 09:00:12 +0300

    Add files via upload

---
+++ advanced_JS_5/app.js
@@ -0,0 +1,22 @@
+
+	function areAnagram(str1,str2)
+	{
+
+		let n1 = str1.length;
+		let n2 = str2.length;
+
+
+		if (n1 != n2)
+			return false;
+
+		// Sort both strings
+		str1.sort();
+		str2.sort()
+
+		// Compare sorted strings
+		for (let i = 0; i < n1; i++)
+			if (str1[i] != str2[i])
+				return false;
+
+		return true;
+	}
