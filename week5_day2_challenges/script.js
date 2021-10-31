04ccdfb806d8fce664542c6776c6837fa2547246
Uzi-Cohen <84247101+Uzi-Cohen@users.noreply.github.com>
2021-10-31 11:26:01 +0200

    Add files via upload

---
+++ week5_day2_challenges/script.js
@@ -0,0 +1,24 @@
+// ex 1
+// const getData = () => {
+// 	let xhr = new XMLHttpRequest();
+// 	xhr.open('GET', 'https://api.giphy.com/v1/gifs/search?q=hilarious&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My');
+// 	xhr.send();
+// 	xhr.onload = function() {
+// 		console.log(JSON.parse(xhr.response))
+// 	}
+// }
+//
+//
+// getData();
+
+//#2
+// const getData = () => {
+// 	let xhr = new XMLHttpRequest();
+// 	xhr.open('GET', 'https://api.giphy.com/v1/gifs/search?q=sun&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My&limit=10&offset=2');
+// 	xhr.send();
+// 	xhr.onload = function() {
+// 		console.log(JSON.parse(xhr.response))
+// 	}
+// }
+//
+// getData();
